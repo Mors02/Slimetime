b19bcd4eb2bbcc4a2b83ea16a46bfe2a4be1ddd5
Leonardo Morselli <leonardo.morselli@bonobostudio.it>
2025-11-26 17:45:36 +0100

    feat(movement): started movement

--- Assets/Settings/Scripts/PlayerMovement.cs
+++ Assets/Settings/Scripts/PlayerMovement.cs
@@ -9,22 +9,81 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float _speed;
 
-    [Range(0, 50)]
+    [Range(50, 600)]
     [SerializeField]
     float _jumpForce;
 
+    [Range(0.1f, 1f)]
+    [SerializeField]
+    float _maxJumpTime, _minJumpPercentage;
+    float _jumpPercentage;
 
-    
+    Vector2 _movement;
+
+    Rigidbody2D _rb;
+
+    bool _grounded, _jumpPressed;
+
+    [SerializeField]
+    Transform _groundCheck;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        this._rb = GetComponent<Rigidbody2D>();
+        _jumpPercentage = _minJumpPercentage;
+    }
+
+    public void Update()
+    {
+        //if we're in air we can descend
+        _rb.position = _rb.position + new Vector2(_movement.x * Time.deltaTime * _speed, _grounded ? 0 : Mathf.Min(0, _movement.y) * Time.deltaTime * _speed);
+        if (_jumpPressed)
+        {
+            _jumpPercentage += Time.deltaTime;
+            Mathf.Clamp(_jumpPercentage, 0, _maxJumpTime);
+        }
+        //_rb.AddForce(new Vector2(_movement.x * Time.deltaTime * _speed, 0));
+        // Debug.Log(_movement);
     }
 
     public void Move(CallbackContext context)
     {
-        Vector2 movement = context.action.actionMap["Move"].ReadValue<Vector2>();
+        _movement = context.action.actionMap["Move"].ReadValue<Vector2>();
+    }
 
-        Debug.Log(movement);
+    public void Jump(CallbackContext context)
+    {
+        if (context.control.IsPressed())
+        {
+            _jumpPressed = true;
+        }
+        else
+        {
+            if (_grounded)
+            {
+
+
+            
+                    Debug.Log(_jumpPercentage);
+                    float percentage = Mathf.Clamp(_jumpPercentage / _maxJumpTime, 0, 1);
+                    _rb.AddForce(new Vector2(0, _jumpForce * percentage));
+                    _jumpPercentage = _minJumpPercentage;
+                    _jumpPressed = false;
+            }
+        }
+  
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+            _grounded = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+            _grounded = false;
     }
 }
